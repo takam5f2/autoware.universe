fb3a27da38bc9b383a24711b4c9c256841b3f818
Takayuki AKAMINE <takayuki.akamine@tier4.jp>
2024-02-09 10:35:21 +0900

    add some functions
    Signed-off-by: Takayuki AKAMINE <takayuki.akamine@tier4.jp>

--- planning/kinematic_reader/src/kinematic_reader.cpp
+++ planning/kinematic_reader/src/kinematic_reader.cpp
@@ -9,7 +9,7 @@ KinematicReader::KinematicReader(const rclcpp::NodeOptions & options)
 : Node ("kinematic_reader", options)
 {
     // Create a subscription to the odometry topic
-
+    // but do not execute the callback
     auto no_executed = [this](const nav_msgs::msg::Odometry::ConstSharedPtr msg) -> void {
       assert(false);
       current_kinematics_ = msg;
@@ -24,6 +24,9 @@ KinematicReader::KinematicReader(const rclcpp::NodeOptions & options)
         rclcpp::CallbackGroupType::MutuallyExclusive, false);
     auto subscription_options = rclcpp::SubscriptionOptions();
     subscription_options.callback_group = cb_group_noexec;
+
+    // Set the topic statistics options
+    subscription_options.topic_stats_options.state = rclcpp::TopicStatisticsState::Enable;
 
 
     sub_ = this->create_subscription<nav_msgs::msg::Odometry>(
@@ -49,14 +52,21 @@ void KinematicReader::timer_callback()
     if (sub_->take(odom_msg, msg_info)) {
         current_kinematics_ = std::make_shared<nav_msgs::msg::Odometry>(odom_msg);
         RCLCPP_INFO(this->get_logger(), "Received odometry message");
+        RCLCPP_INFO(this->get_logger(), "Frame ID: %s", current_kinematics_->header.frame_id.c_str());
+        RCLCPP_INFO(this->get_logger(), "sec: %d", current_kinematics_->header.stamp.sec);
         RCLCPP_INFO(this->get_logger(), "Position: x = %f, y = %f, z = %f",
             current_kinematics_->pose.pose.position.x,
             current_kinematics_->pose.pose.position.y,
             current_kinematics_->pose.pose.position.z);
+        if (msg_info.get_rmw_message_info().from_intra_process) {
+            RCLCPP_INFO(this->get_logger(), "Intra process");
+        }
+        else {
+            RCLCPP_INFO(this->get_logger(), "Inter process");
+        }
     }
     else {
         RCLCPP_INFO(this->get_logger(), "No odometry message received");
-        current_kinematics_ = std::make_shared<nav_msgs::msg::Odometry>(odom_msg);
     }
 }
 
